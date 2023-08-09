54fe262ad46746191dbce3d083ecc89906ab669e
yangfeng <feng.yang@runzeft.com>
2023-08-09 10:41:26 +0800

    mq

--- spring-boot-activemq-producer/src/main/java/com/lance/mq/producer/service/ProducerService.java
+++ spring-boot-activemq-producer/src/main/java/com/lance/mq/producer/service/ProducerService.java
@@ -1,27 +1,27 @@
 package com.lance.mq.producer.service;
 
+import com.alibaba.fastjson.JSON;
+import com.lance.mq.producer.info.OrderInfo;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-import org.apache.activemq.command.ActiveMQQueue;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
-import org.springframework.stereotype.Component;
-
-import com.alibaba.fastjson.JSON;
-import com.lance.mq.producer.info.OrderInfo;
-
 @Component
 public class ProducerService {
 	Logger log = LogManager.getLogger(getClass());
-	@Autowired
+
+	@Resource
 	private JmsTemplate jmsTemplate;
-	@Autowired
+	@Resource
 	private ActiveMQQueue helloQueue;
 	
 	/**
